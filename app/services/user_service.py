e3a30bdfd07c84c0d3b1a7d6b26021f82c48ae65
stev97uce <estibenabc@gmail.com>
2025-06-24 20:16:54 -0500

    Actions

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,8 +1,11 @@
 import httpx
 from app.schemas.user import UserCreate
+from app.schemas.user import UserUpdate
 from app.models.user import User
 from app.core.database import database
 from sqlalchemy import insert
+from sqlalchemy import update
+from sqlalchemy import select
 
 async def check_user_exists(email: str):
     url = f"http://34.225.179.191:8000/check_user_exists"
@@ -60,3 +63,19 @@ async def delete_user_in_auth_service(email: str):
 
     if response.status_code != 200:
         raise Exception(f"Error al eliminar usuario de auth-service: {response.status_code} - {response.text}")
+    
+async def update_user(user_id: int, update_data: UserUpdate):
+    values = update_data.dict(exclude_unset=True)
+
+    query = (
+        update(User)
+        .where(User.id == user_id) 
+        .values(**values)
+        .returning(User)
+    )
+
+    return await database.fetch_one(query)
+
+async def get_user_by_id(user_id: int):
+    query = select(User).where(User.id == user_id)
+    return await database.fetch_one(query)
